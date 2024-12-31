a5ee9b69de3850f767a724ca59389d35ebda0cfa
BMSOHAN <fazlaelahisohan@gmail.com>
2024-12-31 20:40:41 +0600

    BCTlec006.c

---
+++ BCTlec006.c
@@ -0,0 +1,51 @@
+                    //BCT Lecture 06
+                      //... scanf ...
+
+         /*int x;
+            scanf ("%d",&x);//include '&'..
+
+                char my_char;
+                   scanf ("%c",&my_char);
+
+                      float my_float;
+                        scanf ("%f",&my_float);
+
+                         double my_double;
+                           scanf ("%lf",&my_double);*/
+
+    #include<stdio.h>
+       int main (){
+
+
+       /* int a;
+         scanf ("%d",&a);
+          printf("your value:%d \n",a); //must use %d(for 'int')....*/
+
+
+
+
+       /* float x,y;
+         printf ("Enter value for x&y: \n");
+          scanf ("%f%f",&x,&y);
+            printf ("you entered: %.2f and %.3f \n",x,y); */
+
+
+                    // problem 01....
+
+        int a,b,c;
+
+         scanf ("%d%d%d",&a,&b,&c);
+
+          float average = (a+b+c)/3.00;
+
+          printf("value of a:%d\n",a);
+          printf("value of b:%d\n",b);
+          printf("value of c:%d\n",c);
+          printf("value of average:%.2f\n",average);
+
+
+                // ///////////////// //
+
+    return 0;
+
+}
