afe59b5bfde3944cba2d13ce29471a50a8e2e64e
BMSOHAN <fazlaelahisohan@gmail.com>
2024-12-31 20:46:25 +0600

    BCTlec09.c

---
+++ BCTlec09.c
@@ -0,0 +1,110 @@
+                //BCT Lecture 09
+                  //Condition - switch Case...
+                    // one case one time only,,,
+
+     /*   switch(n){// "n" is the main culprit....(must in constant fig)
+       case case_1: //must use ':'
+         //code to execute if n==case_1
+           break; // break...work for 1st case then break.
+       case case_2:
+          //code to execute if n==case_2
+           break; // break...work for 2nd case then break.
+           ....
+
+       case case_n:
+         //code to execute if n==case_n
+           break;
+       default:
+        //default..**(if no case matches)**
+
+
+        } */
+
+      #include<stdio.h>
+        int main (){
+
+
+   /* int x=3;//x=1,,this why the out put will "vlaue is 1"
+                //if x=2,,the out put will "sohan and the value is 2"
+                    // ..................
+
+      switch(x){
+
+   case 1:
+       printf("vlaue is 1\n");
+         break;
+   case 2:
+        printf("sohan and the value is 2\n");
+          break;
+   case 3:
+          printf("adnan and the value is 3\n");
+          printf("  Hi!!!!\n");
+            break;
+    case 3+5:
+         printf("Ha ha ha \n");
+              break;
+
+   default:
+      printf("There is no value\n");
+
+            } */
+
+
+
+
+     /*  char ch='b';
+
+        switch(ch){
+
+     case 'a':
+        printf("The alph is: a\n");
+          break;
+     case '1':
+        printf("The number is: 1\n");
+          break;
+     case 'b':
+        printf("The number is b \n");
+          break;
+     default:
+        printf("ERROR!!!!!\n");
+
+        } */
+
+
+            // if else to switch case......
+
+                // if else condition....
+      /*  int a=3;
+         if (a==1){
+            printf("hi\n");
+
+         }else if(a==2){
+             printf("hello\n");
+         }else if (a==3){
+              printf("good\n");
+         }else{
+                printf("Good Bye...\n");
+         }*/
+
+                // switch case....
+
+       /* int a=3;
+
+         switch(a){
+       case 1:
+        printf("hi\n");
+        break;
+       case 2:
+         printf("hello\n");
+          break;
+       default:
+        printf("good bye");
+
+         } */
+
+
+
+    return 0;
+           }
+
+
