d955fd749d36d2c49027bc9d9a4dd315b9fea380
BMSOHAN <fazlaelahisohan@gmail.com>
2024-12-31 20:37:31 +0600

    BCTlec03.c

---
+++ BCTlec03.c
@@ -0,0 +1,25 @@
+                        //BCTlecture 03
+
+#include<stdio.h>
+int main (){
+
+/*int a;
+int b;
+a=10;
+b=3;
+ printf("Resul is : %d\n",a % b);//% is like remainder = modulus....*/
+
+
+int a=459834644359043;
+long long int b=6543582743659435;
+
+   printf("%d\n",a);
+   printf("%lld\n",b);//used long long int...& the out put is same,,,
+
+
+   printf("%d\n",sizeof(int));//is 4...(4byte),,,,
+
+
+
+return 0;
+}
