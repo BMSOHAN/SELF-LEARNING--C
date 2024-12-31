2840db69dab1d4ef470a882c08ce559ae169e886
BMSOHAN <fazlaelahisohan@gmail.com>
2024-12-31 20:39:31 +0600

    Create BCTlec05.c

---
+++ BCTlec05.c
@@ -0,0 +1,59 @@
+           //BCTlecture05
+
+        // character & ASCII Value.....
+             // 1 byte....(character)
+               //[ Always one '.,' [Ex..{'xy'}] ]
+                        // character= %c
+
+         #include<stdio.h>
+            int main (){
+
+
+
+      /* char my_char;
+
+         my_char = 'S';
+
+            printf ("sohan=%c\n",my_char);*/
+
+
+
+    /*char first='S',second='O',thrd='H',frt='A',fth='N';
+
+        printf ("\t%c%c%c%c%c\n\n",first,second,thrd,frt,fth); //jto gula char toto gula %c......*/
+
+
+
+                        //.....ASCII Value.....
+
+                           //(American Standard Code for Information Interchange)..
+
+                                    // A = 65
+                                    // a = 97
+
+
+
+
+               /* // The way To Find ASCII CODE...
+
+                char  x = 'A';
+
+                   printf ("\t %d\n",x); // Must use %d (integer...)*/
+
+
+
+                //Another way....
+
+
+                int a = 65, b = 66,c = 67;
+
+                  printf ("%c%c%c\n\n",a,b,c);
+
+
+
+
+
+
+          return 0;
+
+                         }
