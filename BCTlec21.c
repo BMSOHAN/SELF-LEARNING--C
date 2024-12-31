701a8c8f012e763d77d34c2065cd59ef7fc3eae5
BMSOHAN <fazlaelahisohan@gmail.com>
2024-12-31 21:05:17 +0600

    BCTlec21.c

---
+++ BCTlec21.c
@@ -0,0 +1,59 @@
+                //BCT Lecture 21
+                //Function Exercise in C
+
+  #include<stdio.h>
+
+                    //Prob 1
+
+   /* void printf_message(){
+
+         printf("Hello! I'm from inside a function.\n");
+    }
+
+ int main(){
+    printf_message();
+
+
+  return 0;
+ } */
+
+                    //Prob 2
+
+
+   /*  void even_or_odd(int x){
+        if (x % 2 == 0){
+            printf("EVEN\n");
+        } else{
+           printf("ODD\n");
+        }
+     }
+
+      int main(){
+        even_or_odd(10);
+
+        return 0;
+
+      } */
+
+                    //Prob 3
+
+
+   /* void print_divisors(int x){
+
+         int i;
+          for(i = 1; i<= x; i++){
+            if(x % i == 0){
+                printf("%d\n", i);
+            }
+
+          }
+
+    }
+
+    int main(){
+
+      print_divisors(20);
+
+    return 0;
+    } */
+
