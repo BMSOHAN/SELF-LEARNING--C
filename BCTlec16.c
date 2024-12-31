ce6ec8980c1994701056c30dde12b014024e91ba
BMSOHAN <fazlaelahisohan@gmail.com>
2024-12-31 20:57:59 +0600

    BCTlec16.c

---
+++ BCTlec16.c
@@ -0,0 +1,41 @@
+               //BCT Lecture 16
+                // LOOP Practice problem...
+
+
+      #include<stdio.h>
+            int main (){
+
+   /* 12345
+      1234
+      123
+      12
+      1
+
+     int i,j; // i for per lines, j for the numbers...
+        for(i=5;i>=1;i--){
+            for(j=1;j<=i;j++){
+                printf("%d",j);//*j*
+            }
+            printf("\n");
+        } */
+
+      /* Display A to Z using loop..
+
+        char ch;
+         for(ch='A';ch<='Z';ch++){
+            printf("%c ",ch);
+
+         } */
+
+
+       /* square sum : 5 to 25
+
+        int i,sum=0;
+          for(i=5;i<=25;i++){
+            sum = sum +(i*i);
+          } printf("%d\n",sum); */
+
+
+    return 0;
+
+            }
