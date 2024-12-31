e287d0ee458c7fae85fab3dd29214f6bc0f6123c
BMSOHAN <fazlaelahisohan@gmail.com>
2024-12-31 20:52:40 +0600

    BCTlec13.c

---
+++ BCTlec13.c
@@ -0,0 +1,38 @@
+                // BCT Lecture 13
+                 // Loop: do while loop
+
+
+      /*  initialization
+            do {        //*Always execute or Print out first*
+                //code to execute
+                 increment/decrement
+            } while(condition); */
+
+      #include<stdio.h>
+         int main (){
+
+   /* int i;
+     i=1;
+     do{
+        printf("Let me go!\n");
+        i = i + 1;
+     } while(i <=2);
+
+       int i;
+       i= -10;
+       do{
+        printf("Sohan\n");
+        i = i - 3;
+       }  while(i>=-12); */
+
+
+    // print 100,95,90,85,......0
+        int i;
+        i = 100;
+         do{
+            printf("%d\n",i);
+            i=i-5;
+         } while(i >= 0);
+
+  return 0;
+          }
