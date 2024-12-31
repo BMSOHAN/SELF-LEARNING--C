10a1fc1f88d105ee8cc894c168411e90969ece06
BMSOHAN <fazlaelahisohan@gmail.com>
2024-12-31 20:35:30 +0600

    BCTlec02.c

---
+++ BCTlec02.c
@@ -0,0 +1,49 @@
+                    //BCTlecture02
+
+#include<stdio.h>
+int main(){
+
+/*printf ("My salary is %d taka.\n",10000000);
+
+printf (" the hash character %c\n",'#');
+
+
+int x=4;
+int y=9;
+printf ("%d + %d =%d\n",x,y,x+y);
+
+x=x*y-6;
+
+printf ("My final value of x is:%d\n",x);
+
+
+int a;
+int b;
+
+a=10;
+b=22;
+printf ("the value %d\n",a+b);*/
+
+/*char ch;
+ch='x';
+
+printf ("the char is...%c\n",ch);
+ch='M';
+printf ("the char2 is...%c\n",ch);*/
+
+
+/*float x;
+x=56.67676;
+float y=34.9098;
+
+printf ("Addition: %.3f\n",x+y);*/
+
+/*int m,n,k;
+m=4;n=5;k=m*n;
+printf (" show the value %d\n",k);*/
+
+
+
+
+return 0;
+}
