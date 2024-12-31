273106494f662074d87ba849bd062a044ad38bf6
BMSOHAN <fazlaelahisohan@gmail.com>
2024-12-31 20:51:32 +0600

    BCTlec12.c

---
+++ BCTlec12.c
@@ -0,0 +1,54 @@
+            // BCT Lecture 12
+              // Loop: While loop
+
+     /*  initialization
+        while(condition){
+            //code to execute
+             increment/decrement
+        }
+
+            For example:
+                    int i=10;
+                    while (i < 12){
+                        printf("Let me go!!\n");
+                        i++;
+                    } */
+
+
+   #include<stdio.h>
+      int main(){
+
+   /* int x=1;
+     while(x <= 3){            // x=1,true
+         printf("Hi\n");       // x=1+2=3,true
+        printf("Hello\n");     // x=3+2=5,false
+
+        x = x+2;
+     }
+
+
+      int x=20;
+        while (x >= 15){
+            printf("sohan\n");
+
+            x = x-2;
+        }
+
+         while(1){ //Infinite Loop....[only true condition]
+            printf("Sohan\n");
+            printf("Adnan\n");
+        } */
+
+       // Print all the Positive numbers from 1 to 100
+
+      int i = 1;
+       while(i <= 100){
+         printf("%d\n",i);
+          i++;
+       }
+
+        // for even numbers : i = 2; i=i + 2;
+          // for odd numbers : i = 1; i=i + 2;
+
+  return 0;
+          }
